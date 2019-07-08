439cdc2852ae3d60641a45b0fbbc64ef5f1b0d02
Morgan Fendrix <siegfried.fendrix@gmail.com>
2019-07-08 15:34:38 +0200

    Commit 0.07 du 08/07/2019 - Utilisation d'une boucle while

--- src/learn_java_boucles/Learn_Java_Boucles.java
+++ src/learn_java_boucles/Learn_Java_Boucles.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package learn_java_boucles;
+import java.util.Scanner; // Importation de la classe Scanner
 
 /**
  *
  * @author Morgan Fendrix
  */
-public class Learn_Java_Boucles {
+public class Learn_Java_Boucles 
+{
+    public static void main(String[] args)
+    {
+        String name; // Declaration variable type String
+        char answer = 'O'; // Declaration variable type char, initialisation de celle-ci
+        Scanner sc = new Scanner(System.in);
+        
+        while(answer == 'O') // Tant que answer est egal a "O"
+        {
+            System.out.println("Donnez un prénom :");
+            name = sc.nextLine();
+            System.out.println("Bonjour " + name + ", espèce de moche !");
+            System.out.println("Voulez-vous réessayer ? (O/N)");
+            answer = sc.nextLine().charAt(0);
+        }
+        System.out.println("Salut pauvre tâche !");
+        
+    }
     
 }
