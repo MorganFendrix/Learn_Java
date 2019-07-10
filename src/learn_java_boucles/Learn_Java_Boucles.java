01f0f0b509ece89bcf05cfc0529eec126bc1aee1
Morgan Fendrix <siegfried.fendrix@gmail.com>
2019-07-10 13:33:18 +0200

    Commit 0.08 du 10/07/2019 - Apprentissage des boucles while, do...while, et for

--- src/learn_java_boucles/Learn_Java_Boucles.java
+++ src/learn_java_boucles/Learn_Java_Boucles.java
@@ -14,6 +14,10 @@ public class Learn_Java_Boucles
 {
     public static void main(String[] args)
     {
+        /*
+                                Premiere methode (while)
+        */
+        
         String name; // Declaration variable type String
         char answer = 'O'; // Declaration variable type char, initialisation de celle-ci
         Scanner sc = new Scanner(System.in);
@@ -23,11 +27,52 @@ public class Learn_Java_Boucles
             System.out.println("Donnez un prénom :");
             name = sc.nextLine();
             System.out.println("Bonjour " + name + ", espèce de moche !");
-            System.out.println("Voulez-vous réessayer ? (O/N)");
-            answer = sc.nextLine().charAt(0);
+            answer = ' ';
+            
+            while(answer != 'O' && answer != 'N')
+            {
+                System.out.println("Voulez-vous réessayer ? (O/N)");
+                answer = sc.nextLine().charAt(0);
+            }
         }
         System.out.println("Salut pauvre tâche !");
         
+        /*
+                                Deuxieme methode (do - while)
+        */
+        
+        String age;
+        char answer2 = ' ';
+        Scanner sc2 = new Scanner(System.in);
+        
+        do
+        {
+            System.out.println("Donnez votre âge :");
+            age = sc2.nextLine();
+            System.out.println("Vous avez " + age + " ans");
+            
+            do
+            {
+                System.out.println("Voulez-vous réessayer ? (O/N)");
+                answer2 = sc.nextLine().charAt(0);
+            }while(answer2 != 'O' && answer2 != 'N');
+        }while(answer2 == 'O');
+        
+        /*
+                                Troisieme methode (for)
+        */
+        
+        for(int test = 1;test <= 10;test++)
+        {
+            System.out.println("Voici la ligne " + test);
+        }
+        
+        for(int test1 = 10;test1 >= 0;test1--)
+            System.out.println("Il reste " + test1 + "ligne(s) à écrire");
+        
+        for(int test2 = 0, test3 = 2;(test2 < 10 && test3 < 6); test2++, test3+=2)
+        {
+            System.out.println("test2 = " + test2 + ", test3 = " + test3);
+        }
     }
-    
 }
