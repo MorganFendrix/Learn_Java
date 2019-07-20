08308dbd3f9c1d7459ddb37a112d89eaab784704
Morgan Fendrix <siegfried.fendrix@gmail.com>
2019-07-20 22:20:27 +0200

    Commit 0.09 du 20/07/2019 - Ajout du TP1

---
+++ src/TP1/tp1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Morgan Fendrix
+ */
+public class tp1 
+{
+    public static void main(String[] args)
+    {
+        int loop = 1;
+        while (loop == 1)
+        {
+            System.out.println("Bonjour !");
+            System.out.println("Quel conversion souhaitez-vous faire ?");
+            System.out.println("Celsius vers Fahrenheit ? Faites 1 ");
+            System.out.println("Farenheit vers Celsius ? Faites 2 ");
+        }
+    }
+    
+}
